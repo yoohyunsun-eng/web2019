76735add9418e662384352fb1ff369a136bc5c41
yoohyunsun-eng <57124892+yoohyunsun-eng@users.noreply.github.com>
2019-12-04 11:31:52 +0900

    Create welcome.php

---
+++ html/20191113_form/welcome.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta charset="UTF-8">
+	<title>welcome</title>
+</head>
+<body>
+	<?
+	$name = $_GET["myname"];
+	$email = $_GET["email"];
+	?>
+
+	이름 : <?echo $name;?>님 반갑습니다.<br>
+	이메일 : <?echo $email;?>
+</body>
+</html>
